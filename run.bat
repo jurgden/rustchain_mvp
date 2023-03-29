2ab93dd55a18729c6609b1c30b945ba9d06b7ab9
Jordan T. Reed <82841924+jurgden@users.noreply.github.com>
2023-03-28 21:25:02 -0700

    final changes for the night, got a tui working

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+
+REM Open a new terminal window and run the TUI
+start cmd.exe /k "cargo run"
--- src/ui.rs
+++ src/ui.rs
@@ -1,42 +1,102 @@
+use crossterm::event::{read, Event, KeyCode};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use std::io::{self};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 use tui::backend::CrosstermBackend;
-// use tui::layout::{Constraint, Direction, Layout};
-// use tui::style::Modifier;
-// use tui::text;
-use tui::widgets::{Block, Borders};
+use tui::layout::{Constraint, Direction, Layout};
+use tui::style::{Color, Modifier, Style};
+use tui::text::{Span, Spans};
+use tui::widgets::{Block, Borders, List, ListItem};
 use tui::Terminal;
 
 pub fn run_tui() -> Result<(), Box<dyn std::error::Error>> {
     // Initialize the terminal backend
-    let stdout = io::stdout().into_raw_mode()?;
+    enable_raw_mode()?;
+    let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    // Enter the main loop
+    let vibrant_border_style = Style::default().fg(Color::Rgb(50, 200, 50));
+
+    let menu_items = [
+        "Check balance",
+        "Send transaction",
+        "Create new address",
+        "View transaction history",
+        "Exit",
+    ];
+
+    let mut active_menu_item = 0;
+
     loop {
-        // Draw the UI
         terminal.draw(|f| {
             let size = f.size();
-            let block = Block::default()
-                .title("Rustchain MVP")
-                .borders(Borders::ALL);
-            f.render_widget(block, size);
+            let layout = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints([Constraint::Percentage(100)].as_ref())
+                .split(size);
+
+            let menu_block = Block::default()
+                .title(Spans::from(Span::styled(
+                    "Rustchain MVP - Wallet",
+                    vibrant_border_style,
+                )))
+                .borders(Borders::ALL)
+                .border_style(vibrant_border_style);
+
+            let menu_items: Vec<ListItem> = menu_items
+                .iter()
+                .enumerate()
+                .map(|(i, item)| {
+                    let style = if i == active_menu_item {
+                        Style::default().add_modifier(Modifier::REVERSED)
+                    } else {
+                        Style::default()
+                    };
+                    ListItem::new(Spans::from(Span::styled(*item, style)))
+                })
+                .collect();
+
+            let menu_widget = List::new(menu_items)
+                .block(menu_block)
+                .highlight_style(Style::default().bg(Color::DarkGray));
+            f.render_widget(menu_widget, layout[0]);
         })?;
 
         // Handle input
-        let stdin = std::io::stdin();
-        for key in stdin.keys() {
-            match key? {
-                Key::Char('q') => break, // Quit on 'q'
-                _ => continue,
+        match read()? {
+            Event::Key(event) => {
+                match event.code {
+                    KeyCode::Up => {
+                        if active_menu_item > 0 {
+                            active_menu_item -= 1;
+                        }
+                    }
+                    KeyCode::Down => {
+                        if active_menu_item < menu_items.len() - 1 {
+                            active_menu_item += 1;
+                        }
+                    }
+                    KeyCode::Enter => {
+                        // Perform the action based on the selected menu item
+                        match active_menu_item {
+                            0 => println!("Checking balance..."),
+                            1 => println!("Sending transaction..."),
+                            2 => println!("Creating new address..."),
+                            3 => println!("Viewing transaction history..."),
+                            4 => break,
+                            _ => {}
+                        }
+                    }
+                    KeyCode::Char('q') | KeyCode::Esc => break,
+                    _ => {}
+                }
             }
+            _ => {}
         }
     }
 
     // Clean up and exit
-    // terminal.show_cursor()?;
-    // Ok(())
+    disable_raw_mode()?;
+    Ok(())
 }
